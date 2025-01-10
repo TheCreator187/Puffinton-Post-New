5f76d5600a3472e22e823033a4d98ae30cb2a2c8
thecreator187 <fleegangstudios@gmail.com~>
2025-01-09 21:40:03 -0500

    modified:   blog.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Initialize slides
+  $('.slides').slidesjs({
+    width: 940,
+    height: 528,
+    navigation: false,
+    play: {
+      active: true,
+      auto: true,
+      interval: 4000,
+      swap: true
+    }
+  });
+
+  // Add any additional initialization code here
+});
